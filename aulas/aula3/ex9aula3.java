6ff6405fb7a63b5e3ad7f13dc0d21aa836e413ef
girardiarthur <girardiarthur11@gmail.com>
2026-09-02 20:21:42 -0300

    ex8 feito + comentarios

---
+++ aulas/aula3/ex9aula3.java
@@ -0,0 +1,12 @@
+//Faça um algoritmo para apresentar:
+//– o cálculo da área de um quadrado de 350 metros de lado.
+
+package aula3;
+public class ex9aula3 {
+    public static void main (String[] args){
+        int lado = 350;
+        int area = lado * lado;
+        System.out.println("A área do quadrado é: " + area + " metros");
+
+    }
+}
