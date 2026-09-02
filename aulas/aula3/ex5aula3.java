4e0e7725fbbfd62ed0aa9eb9d09ff0117ebec577
girardiarthur <girardiarthur11@gmail.com>
2026-09-02 19:32:53 -0300

    ex5 feito

--- aulas/aula3/ex5aula3.java
+++ aulas/aula3/ex5aula3.java
@@ -1,6 +1,16 @@
 package aula3;
+import java.util.Scanner;
 public class ex5aula3 {
     public static void main (String[] args){
+    Scanner entrada = new Scanner(System.in);
+    System.out.println("Digite sua primeira nota: ");
+    double nota1 = entrada.nextDouble();
+    System.out.println("Digite sua segunda nota: ");
+    double nota2 = entrada.nextDouble();
+    System.out.println("Digite sua terceira nota: ");
+    double nota3 = entrada.nextDouble();
+    double media = (nota1 + nota2 + nota3) / 2;
+    System.out.printf("Sua média é de : %.2f",media);
 
     }
 }
