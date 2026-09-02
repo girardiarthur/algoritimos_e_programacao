e1af0b02c2ee4ca0b44a44a46f730b62b3bb4196
girardiarthur <girardiarthur11@gmail.com>
2026-09-02 19:53:39 -0300

    ex6 feito

---
+++ aulas/aula3/ex6aula3.java
@@ -0,0 +1,16 @@
+package aula3;
+
+public class ex6aula3 {
+    public static void main (String[] args){
+        double conta1 = (20 - 15) /2.0 ;
+        double conta2 = Math.pow(2, (5.0/20.0)) + 30.0/(Math.pow(15,2));
+        double conta3 = 35.0/(6+2);
+        double conta4 = 23%4;
+        System.out.printf("O RESULTADO DA OPERAÇÃO 1 É : %.2f %n", conta1);
+        System.out.printf("O RESULTADO DA OPERAÇÃO 2 É : %.2f %n", conta2);
+        System.out.printf("O RESULTADO DA OPERAÇÃO 3 É : %.2f %n", conta3);
+        System.out.printf("O RESULTADO DA OPERAÇÃO 4 É : %.2f %n", conta4);
+
+        }
+    }
+
