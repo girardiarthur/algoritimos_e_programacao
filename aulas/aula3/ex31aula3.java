25ecb70bf34e652482305559b09d6c1df3644852
girardiarthur <girardiarthur11@gmail.com>
2026-09-02 14:15:47 +0000

    ex4 feito

---
+++ aulas/aula3/ex31aula3.java
@@ -0,0 +1,15 @@
+package aula3;
+import java.util.Scanner;
+public class ex31aula3{
+    public static void main(String[] args) {
+        Scanner entrada = new  Scanner(System.in);
+        System.out.println("Digite o primeiro número inteiro");
+        int numero1 = entrada.nextInt();
+        System.out.println("Digite o segundo número inteiro");
+        int numero2 = entrada.nextInt();
+        int resultado = numero1 * numero2;
+        System.out.println("O produto entre os números digitados é: " + resultado);
+        entrada.close();
+
+    }
+}
---
+++ aulas/aula3/ex4aula3.java
@@ -0,0 +1,20 @@
+package aula3;
+import java.util.Scanner;
+public class ex4aula3 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite o primeiro número");
+        int numero1 = entrada.nextInt();
+        System.out.println("Digite o segundo número");
+        int numero2 = entrada.nextInt();
+        System.out.println("Digite o terceiro número");
+        int numero3 = entrada.nextInt();
+        System.out.println("Digite o quarto número");
+        int numero4 = entrada.nextInt();
+        int resultado = numero1 + numero2 + numero3 + numero4;
+        System.out.printf("a soma dos números é: %d", resultado);
+        
+
+
+    }
+}
