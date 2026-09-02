13cc65279b72b7215c9bf4f16d3b50c7f5a7c971
girardiarthur <girardiarthur11@gmail.com>
2026-09-02 20:26:45 -0300

    ex 9 e 10 feitos

---
+++ aulas/aula3/ex10aula3.java
@@ -0,0 +1,13 @@
+//Construir um algoritmo para imprimir:
+//o cálculo da área de um círculo com raio de 5 cm. (π = 3.14159)
+//área do círculo = π ˆ r2
+
+package aula3;
+public class ex10aula3 {
+    public static void main (String []args){
+        int raio = 5;
+        double pi = Math.PI ;
+        double area = pi * Math.pow(raio, 2);
+        System.out.printf("A área do circulo é: %f centimetros",area);
+    }
+}
